a2abf842bb8baf0aacfbb88d2bb8349120db1934
KKIMIs <ssoox1056@gmail.com>
2022-07-11 21:42:31 +0900

    algorithm d7

--- Algorithm/Part2/BJ_14888.py
+++ Algorithm/Part2/BJ_14888.py
@@ -1,7 +1,7 @@
 #BeakJoon 14888
 #2022-07-10
 
-#연산자 끼워넣기
+#연산자 끼워넣기/DFS/재귀함수
 
 '''
 입력
@@ -19,16 +19,34 @@
 또한, 앞에서부터 계산했을 때, 중간에 계산되는 식의 결과도 
 항상 -10억보다 크거나 같고, 10억보다 작거나 같다.
 
+=> 재귀 함수를 이용한 풀이
+https://chul2-ing.tistory.com/67, https://hbj0209.tistory.com/102
 '''
 
 t = int(input())
-nums = [list(map(int, input().split))]
-opers = [list(map(int, input().split()))]
+nums = list(map(int, input().split()))
+opers = list(map(int, input().split()))
 
-op = ["+", "-", "*", "//"]
-cal = []
+max_ans = -1000000000
+min_ans = 1000000000
 
-for _ in range(t+sum(opers)):
+def cal(val, idx, add, sub, mul, div):
+    global max_ans, min_ans
+    if idx == t:
+        max_ans = max(max_ans, val)
+        min_ans = min(min_ans, val)
+        return 
+
     
+    if add>0:
+        cal(val + nums[idx], idx+1, add-1, sub, mul, div)
+    if sub>0:
+        cal(val - nums[idx], idx+1, add, sub-1, mul, div)
+    if mul>0:
+        cal(val * nums[idx], idx+1, add, sub, mul-1, div)
+    if div>0:
+        cal(int(val/nums[idx]), idx+1, add, sub, mul, div-1)
 
-
+cal(nums[0], 1, opers[0],opers[1],opers[2],opers[3])
+print(max_ans)
+print(min_ans)
